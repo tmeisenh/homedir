8f1cd750efb0abf22ae6b6d58cbabe43cc11a771
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2014-10-21 21:00:02 -0500

    Added script to fix home and end keys on osx. -Travis

---
+++ bin-darwin/fix_home_end_keys.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ ! -d $HOME/Library/KeyBindings ]] 
+then
+  mkdir $HOME/Library/KeyBindings
+fi
+
+if [[ ! -e DefaultKeyBinding.dict ]]
+then
+  echo "{
+  /* Remap Home / End keys to be correct */
+  "\UF729" = "moveToBeginningOfLine:"; /* Home */
+  "\UF72B" = "moveToEndOfLine:"; /* End */
+  "$\UF729" = "moveToBeginningOfLineAndModifySelection:"; /* Shift + Home */
+  "$\UF72B" = "moveToEndOfLineAndModifySelection:"; /* Shift + End */
+  "^\UF729" = "moveToBeginningOfDocument:"; /* Ctrl + Home */
+  "^\UF72B" = "moveToEndOfDocument:"; /* Ctrl + End */
+  "$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
+  "$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
+}" > $HOME/Library/KeyBindings/DefaultKeyBinding.dict
+fi
+echo "You must reboot for the changes to take."
