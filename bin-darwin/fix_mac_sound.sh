0fe00488a34105c957dcd35cf8a528f80125008d
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2014-10-22 19:08:49 -0500

    Added bash script that wraps some applescript that fixes my sound issues when kvm switching. -Travis

---
+++ bin-darwin/fix_mac_sound.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Apple Script wrapped in a bash script (for ease of use)
+# that sets the sound output to the headphones.
+# Note: Will require you to authorize the terminal to "control"
+# the mac.
+#
+# Author: Travis B. Meisenheimer
+
+osascript << EOF
+tell application "System Preferences" to activate
+tell application "System Preferences"
+	reveal anchor "output" of pane id "com.apple.preference.sound"
+end tell
+tell application "System Events" to tell process "System Preferences"
+	tell table 1 of scroll area 1 of tab group 1 of window 1
+		select (row 1 where value of text field 1 is "Headphones")
+	end tell
+end tell
+quit application "System Preferences"
+EOF
