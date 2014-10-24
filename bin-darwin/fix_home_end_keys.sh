fbf5fe398c44986c24396dd1363171437237dd46
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2014-10-23 20:07:50 -0500

    Forgot to escape quotes in the keybindings file. (I thought yosemite was causing the problem)

--- bin-darwin/fix_home_end_keys.sh
+++ bin-darwin/fix_home_end_keys.sh
@@ -9,14 +9,14 @@ if [[ ! -e DefaultKeyBinding.dict ]]
 then
   echo "{
   /* Remap Home / End keys to be correct */
-  "\UF729" = "moveToBeginningOfLine:"; /* Home */
-  "\UF72B" = "moveToEndOfLine:"; /* End */
-  "$\UF729" = "moveToBeginningOfLineAndModifySelection:"; /* Shift + Home */
-  "$\UF72B" = "moveToEndOfLineAndModifySelection:"; /* Shift + End */
-  "^\UF729" = "moveToBeginningOfDocument:"; /* Ctrl + Home */
-  "^\UF72B" = "moveToEndOfDocument:"; /* Ctrl + End */
-  "$^\UF729" = "moveToBeginningOfDocumentAndModifySelection:"; /* Shift + Ctrl + Home */
-  "$^\UF72B" = "moveToEndOfDocumentAndModifySelection:"; /* Shift + Ctrl + End */
+  \"\UF729\" = \"moveToBeginningOfLine:\"; /* Home */
+  \"\UF72B\" = \"moveToEndOfLine:\"; /* End */
+  \"$\UF729\" = \"moveToBeginningOfLineAndModifySelection:\"; /* Shift + Home */
+  \"$\UF72B\" = \"moveToEndOfLineAndModifySelection:\"; /* Shift + End */
+  \"^\UF729\" = \"moveToBeginningOfDocument:\"; /* Ctrl + Home */
+  \"^\UF72B\" = \"moveToEndOfDocument:\"; /* Ctrl + End */
+  \"$^\UF729\" = \"moveToBeginningOfDocumentAndModifySelection:\"; /* Shift + Ctrl + Home */
+  \"$^\UF72B\" = \"moveToEndOfDocumentAndModifySelection:\"; /* Shift + Ctrl + End */
 }" > $HOME/Library/KeyBindings/DefaultKeyBinding.dict
 fi
 echo "You must reboot for the changes to take."
