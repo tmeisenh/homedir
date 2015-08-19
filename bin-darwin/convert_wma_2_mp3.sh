a7147052ed15f8a33d107626da023b8efc49d2a6
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2015-08-18 22:54:12 -0500

    Added convert wma2mp3 script -Travis

---
+++ bin-darwin/convert_wma_2_mp3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# remove spaces and replace with _
+for i in *.wma; do mv "$i" `echo $i | tr ' ' '_'`; done
+
+# lowercase everything
+for i in *.[Ww][Mm][Aa]; do mv "$i" `echo $i | tr '[A-Z]' '[a-z]'`; done
+
+# encode as mp3 using ffmpeg
+for i in *.wma; do ffmpeg -i $i -ab 192000 ${i%.wma}.mp3 ; done
