c16731047e1657377c86972347f13adf797d3d0a
Travis 'All-Father' Meisenheimer <tmeisenh@gmail.com>
2015-12-02 20:32:37 -0600

    Added launchd job to update homebrew

---
+++ bin-darwin/update_homebrew_cron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PATH=/usr/local/bin:/usr/bin:/bin
+
+if [ ! -f "$(which brew)" ]; then
+  echo "homebrew is not installed"
+  exit 1
+fi
+
+(brew update && brew upgrade && brew doctor && brew cleanup)
