b366a89145d6ea1104472bd831d1450d65f8309f
Travis Meisenheimer <travis@indexoutofbounds.com>
2016-05-04 20:46:39 -0500

    Added script to create ctags file for ios frameworks

---
+++ bin-darwin/system_ctags.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# updates system ctags file
+
+cd /System/Library/Frameworks
+
+sdks=(
+  CoreData.framework
+  Security.framework
+)
+
+for f in "${sdks[@]}"
+do
+  cd $f
+  ctags --languages=objectivec --langmap=objectivec:.h.m --append -f ~/git/global-objc-tags
+  cd ..
+done
+
+## uikit is elsewhere...
+xcode_path=`xcode-select -p`
+cd /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/UIKit.framework
+ctags --languages=objectivec --langmap=objectivec:.h.m --append -f ~/git/global-objc-tags
