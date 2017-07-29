9185669e5529b33ea69efd032b1812336a52e145
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-07-29 09:19:26 -0500

    Pin versions to what works with flycheck

--- bin-darwin/install_js.sh
+++ bin-darwin/install_js.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-npm install -g tern eslint babel-eslint eslint-plugin-react js-beautify yarn
+npm uninstall -g tern eslint eslint_d babel-eslint eslint-plugin-react 
+npm install -g tern eslint@3.19 eslint_d@4.2.5 babel-eslint@7.2.2 eslint-plugin-react@7.1.0 
