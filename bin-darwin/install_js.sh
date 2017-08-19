ace7df3e0d2cd670bc58369e0270a981d843d2fa
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-08-19 13:36:15 -0500

    fixed install_js

--- bin-darwin/install_js.sh
+++ bin-darwin/install_js.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-npm uninstall -g tern eslint eslint_d babel-eslint eslint-plugin-react 
-npm install -g tern eslint@3.19 eslint_d@4.2.5 babel-eslint@7.2.2 eslint-plugin-react@7.1.0 
+pkgs=(
+tern 
+eslint@3.19 
+eslint_d@4.2.5 
+babel-eslint@7.2.2 
+eslint-plugin-react@7.1.0 
+eslint-config-airbnb@15.1.0 
+eslint-plugin-import@2.7.0 
+eslint-plugin-jsx-a11y@5.1.1 
+eslint-plugin-promise@3.5.0
+)
+
+echo "Installing packages for node version: `node --version`"
+echo "Installing packages from npm version: `npm --version`"
+
+for i in ${pkgs[@]}; do
+  pkg=`echo "${i}" | cut -d @ -f 1`
+  npm uninstall -g ${pkg}
+done
+
+#npm install -g "${pkgs[@]}"
