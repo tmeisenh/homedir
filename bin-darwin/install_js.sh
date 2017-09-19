3d342303e3aa7a43c2c55287b368d49d82dc4611
Travis Meisenheimer <travis@indexoutofbounds.com>
2017-09-19 16:36:08 -0500

    Add mocha plugin

--- bin-darwin/install_js.sh
+++ bin-darwin/install_js.sh
@@ -8,6 +8,7 @@ babel-eslint@7.2.2
 eslint-plugin-react@7.1.0 
 eslint-config-airbnb@15.1.0 
 eslint-plugin-import@2.7.0 
+eslint-plugin-mocha@4.11.0
 eslint-plugin-jsx-a11y@5.1.1 
 eslint-plugin-promise@3.5.0
 )
