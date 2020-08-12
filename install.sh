bc45efd91ea2212af96520514f9538b609b06d16
Travis Meisenheimer <travis@indexoutofbounds.com>
2020-08-12 10:36:28 -0500

    Adds function to install tmux plugins

--- install.sh
+++ install.sh
@@ -11,30 +11,33 @@ cp_cmd="/bin/cp"
 rm_cmd="/bin/rm"
 install-homedir() {
   echo "setting up info/doc directories..."
-	$cp_cmd -a ${SRC}/how-to/ ${DEST}/how-to
+  $cp_cmd -a ${SRC}/how-to/ ${DEST}/how-to
   
   echo "setting up bin directories..."
-	$cp_cmd -a ${SRC}/bin/ ${DEST}/bin
-	$cp_cmd -a ${SRC}/bin-freebsd ${DEST}/bin-freebsd
-	$cp_cmd -a ${SRC}/bin-darwin ${DEST}/bin-darwin
+  $cp_cmd -a ${SRC}/bin/ ${DEST}/bin
+  $cp_cmd -a ${SRC}/bin-freebsd ${DEST}/bin-freebsd
+  $cp_cmd -a ${SRC}/bin-darwin ${DEST}/bin-darwin
 
   echo "setting up dev tools..."
   $cp_cmd -a ${SRC}/git/gitconfig ${DEST}/.gitconfig
   $cp_cmd -a ${SRC}/tmux.conf ${DEST}/.tmux.conf
   $cp_cmd -a ${SRC}/ctags ${DEST}/.ctags
  
-	echo "DONE with install tasks."
+  echo "DONE with install tasks."
 }
-  
+
+tmux_plugins() {
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+}  
 
 uninstall-homedir() {
-	echo "Uninstalling homedir stuff from ${DEST}..."
-	$rm_cmd -rf ${DEST}/bin
-	$rm_cmd -rf ${DEST}/bin-freebsd
-	$rm_cmd -rf ${DEST}/bin-darwin
-	$rm_cmd -rf ${DEST}/how-to
-	$rm_cmd -rf ${DEST}/.gitconfig
-	$rm_cmd -rf ${DEST}/.tmux.conf
+  echo "Uninstalling homedir stuff from ${DEST}..."
+  $rm_cmd -rf ${DEST}/bin
+  $rm_cmd -rf ${DEST}/bin-freebsd
+  $rm_cmd -rf ${DEST}/bin-darwin
+  $rm_cmd -rf ${DEST}/how-to
+  $rm_cmd -rf ${DEST}/.gitconfig
+  $rm_cmd -rf ${DEST}/.tmux.conf
   $rm_cmd -rf ${DEST}/.ctags
 }
 
